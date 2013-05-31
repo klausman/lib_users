b4bc0df433f0ba3af336be5f2bce377525f73546
Tobias Klausmann <klausman@schwarzvogel.de>
2013-05-31 13:42:48 +0200

    Version 0.6

--- lib_users.py
+++ lib_users.py
@@ -18,7 +18,7 @@ PROCFSBASE = "/proc/"
 PERMWARNING = """\
 Warning: Some files could not be read. Note that lib_users has to be run as
 root to get a full list of deleted in-use libraries.\n"""
-__version__ = "0.5"
+__version__ = "0.6"
 
 # These are no true libs so don't make our process a deleted libs user
 NOLIBS = ["/SYSV*", "/dev/zero", "/dev/shm/*", "/drm"]
