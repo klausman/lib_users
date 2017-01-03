0d85ad5d8f1fd240985e43bf830ebc07a7d2b535
Tobias Klausmann <klausman@schwarzvogel.de>
2017-01-03 11:09:40 +0100

    Switch to using setuptools instead of mkdist

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='lib_users',
+      version='0.10',
+      description='Checks /proc for libraries and files being mapped/open '
+		  'but marked as deleted',
+      author='Tobias Klausmann',
+      author_email='klausman@schwarzvogel.de',
+      url='https://github.com/klausman/lib_users',
+      packages=['lib_users_util'],
+      scripts=['lib_users.py', 'fd_users.py'],
+     )
